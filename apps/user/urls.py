0b33179e8d593a267af7168b871fa8a83faaec23
Isabela <imendozam@eafit.edu.co>
2025-09-03 15:49:57 -0500

    correccion urls

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import LoginView, LogoutView, SignupCustomerView, SignupProviderView
 
+app_name = 'users'
+
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
