f058a2c8a9027b38c6c2879acd9177d9f6af48a6
jayounghoyos <jayoungh@eafit.edu.co>
2025-09-03 12:11:26 -0500

    urls: usar namespaces y remover includes duplicados

--- config/urls.py
+++ config/urls.py
@@ -9,15 +9,15 @@ urlpatterns = [
     # Rutas de autenticación y registro (aparecen en /login, /logout, /signup, /providers/signup)
     path('', include(('apps.user.urls', 'users'), namespace='users')),
 
-
     # Core (home y about)
     path('', include(('apps.core.urls', 'core'), namespace='core')),
     
     # Rutas propias de proveedores bajo /providers/...
     path('providers/', include(('apps.providers.urls', 'providers'), namespace='providers')),
 
-    path('', include('apps.core.urls')),
-    path("", include("apps.listings.urls")),
+    #path('', include('apps.core.urls')),
+    #path("", include("apps.listings.urls")),
+    path('listings/', include(('apps.listings.urls', 'listings'), namespace='listings')),
 ]
 
 if settings.DEBUG:
