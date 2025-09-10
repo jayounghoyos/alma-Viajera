2d64bcde57378b238d9853433aee60f5ee8a89df
jayounghoyos <jayoungh@eafit.edu.co>
2025-09-10 09:03:27 -0500

    QR implementation

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,9 +1,12 @@
+# Juan Andrés Young Hoyos
 from django.urls import path
 from django.views.generic import TemplateView
+from . import views
 
 app_name = "core"
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="core/home.html"), name="home"),
     path("about/", TemplateView.as_view(template_name="core/about.html"), name="about"),
+    path("qr-reservation/", views.qr_reservation, name="qr_reservation"),
 ]
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,4 +1,5 @@
-#from django.shortcuts import render
+# Juan Andrés Young Hoyos
+from django.shortcuts import render
 from django.views.generic import TemplateView
 # Create your views here.
 
@@ -6,3 +7,13 @@ from django.views.generic import TemplateView
 class HomeView(TemplateView):
     """ View para el home y que probablemente va a heredarle a los otros"""
     template_name = 'core/home.html'
+
+
+def qr_reservation(request):
+    """
+    Vista para mostrar el código QR de reserva
+    
+    Returns:
+        Página con la imagen QR
+    """
+    return render(request, 'core/qr_reservation.html')
